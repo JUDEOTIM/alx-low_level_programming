a38fd2ddebe3d707d0eeaab7da56de63939aa8ba
JUDEOTIM <otimjudeoj@gmail.com>
2022-11-07 00:04:19 -0800

    a function that returns 1 if a string is a palindrome and 0 if not.

---
+++ 0x03-debugging/jude.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - starts the program
+*returns - zero at the end of the function
+*/
+
+int main(void)
+{
+
+int J = 27;
+for (; J <= 27 ; J--);
+printf("%d\n", J--);
+return 0;
+
+}
